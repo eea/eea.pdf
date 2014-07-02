55a2e6c36daf30157ed24b3069885a9bdbd782cf
Alin Voinea <alin@eaudeweb.ro>
2014-07-02 15:36:15 +0300

    Refs #20267 - Add custom pdf.body for Topic and Collection
    - Add custom pdf.body for Topic and Collection content-types that
      is an aggregated page based on query results pdf.body

---
+++ eea/pdf/themes/classical/collection.py
@@ -0,0 +1,22 @@
+""" PDF View
+"""
+from zope.component import queryMultiAdapter
+from Products.Five.browser import BrowserView
+
+class Body(BrowserView):
+    """ Custom PDF body
+    """
+    @property
+    def pdfs(self):
+        """ Folder children
+        """
+        brains = self.context.queryCatalog(batch=False)
+        ajax_load = self.request.get('ajax_load', False)
+        self.request.form['ajax_load'] = True
+
+        for brain in brains:
+            doc = brain.getObject()
+            pdf = queryMultiAdapter((doc, self.request), name='pdf.body')
+            yield pdf()
+
+        self.request.form['ajax_load'] = ajax_load
