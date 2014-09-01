27d1be9683b99a6affda4903765710cea5d28852
Alin Voinea <alin@eaudeweb.ro>
2014-09-01 19:32:07 +0300

    Refs #20843 - Send download.pdf to the async worker

--- eea/pdf/browser/app/download.py
+++ eea/pdf/browser/app/download.py
@@ -1,14 +1,40 @@
 """ Download PDF
 """
+import sys
 import logging
-from zope.component import queryMultiAdapter
+from pprint import pprint
+from zope.interface import alsoProvides
+from zope.publisher.interfaces import NotFound
+from zope.component import queryMultiAdapter, queryUtility
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
+from plone.app.async.interfaces import IAsyncService
 from eea.converter.browser.app.download import Pdf
 from eea.downloads.interfaces import IStorage
+from eea.pdf.interfaces import ILayer
 from eea.pdf.config import EEAMessageFactory as _
+
+from ZPublisher.HTTPResponse import HTTPResponse
+from ZPublisher.HTTPRequest import HTTPRequest
+
 logger = logging.getLogger('eea.pdf')
 
+def make_async_pdf(context, converter, **kwargs):
+    """ Async job
+    """
+    converter.run()
+    storage = IStorage(context).of('pdf')
+    filepath = storage.filepath()
+    if converter.path:
+        converter.copy(converter.path, filepath)
+    converter.cleanup()
+    return converter.path
+
+def job_failure_callback(result):
+    logger.exception("Failure %s", result)
+
+def job_success_callback(result):
+    logger.info("Success %s", result)
 
 class Download(Pdf):
     """ Download PDF
@@ -44,12 +70,20 @@ class Download(Pdf):
     def download(self, email='', **kwargs):
         """ Download
         """
-        ## ASYNC job
-        # super(Download, self).__call__(**kwargs)
-        logger.warning('Download as PDF ASYNC job not started for %s',
-                       self.context.absolute_url())
-        return self.finish()
+        # Cheat condition @@plone_context_state/is_view_template
+        self.request['ACTUAL_URL'] = self.context.absolute_url()
 
+        converter = self.make_pdf(dry_run=True)
+
+        async = queryUtility(IAsyncService)
+        job = async.queueJob(make_async_pdf,
+            self.context, converter, email=email, **kwargs
+        )
+
+        job.addCallback(job_success_callback)
+        job.addCallbacks(failure=job_failure_callback)
+
+        return self.finish()
 
     def post(self, **kwargs):
         """ POST
@@ -70,6 +104,9 @@ class Download(Pdf):
     def __call__(self, **kwargs):
         support = queryMultiAdapter((self.context, self.request),
                                     name='pdf.support')
+        if not getattr(support, 'can_download', lambda: False)():
+            raise NotFound(self.context, self.__name__, self.request)
+
         email = getattr(support, 'email', lambda: None)()
         if email:
             return self.download(email, **kwargs)
