1509dadba79e0d4593b0d7959bc39a3b11e7a7c2
ichim-david <ichim.david@gmail.com>
2014-11-11 17:57:01 +0200

    * Feature: added coverImage field to folderish content which allows  you to set custom images for the cover page  [ichimd-david refs #21637]

--- eea/pdf/subtypes/schema.py
+++ eea/pdf/subtypes/schema.py
@@ -1,12 +1,17 @@
 """ Schema extender
 """
+from Products.Archetypes.Widget import ImageWidget
+from plone.app.blob.field import ImageField
+
 from zope.interface import implements
 from Products.Archetypes import public
 from archetypes.schemaextender.interfaces import ISchemaExtender
 from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
 from archetypes.schemaextender.field import ExtensionField
+
 from eea.pdf.config import EEAMessageFactory as _
 from eea.pdf.interfaces import ILayer, IPDFTool, IPDFTheme
+
 
 class ExtendedIntegerField(ExtensionField, public.IntegerField):
     """ IntegerField for schema extender
@@ -15,6 +20,10 @@ class ExtendedIntegerField(ExtensionField, public.IntegerField):
 class ExtendedStringField(ExtensionField, public.StringField):
     """ StringField for schema extender
     """
+
+class ExtendedImageField(ExtensionField, ImageField):
+    """ derivative of blobfield for extending schemas """
+
 
 class PDFSchemaExtender(object):
     """ Schema extender
@@ -95,6 +104,17 @@ class CollectionSchemaExtender(PDFSchemaExtender):
                     u"Total maximum children items to be included"
                     u"while generating PDF. Leave it empty to use the portal "
                     u"global defined value."
+                )
+            )
+        ),
+        ExtendedImageField(
+            name='coverImage',
+            schemata='settings',
+            write_permission="Can customize PDF",
+            widget=ImageWidget(
+                label=_(u"PDF cover main image"),
+                description=_(
+                    u"Main cover image that will show up on the pdf cover page"
                 )
             )
         ),
