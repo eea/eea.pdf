102ef7fde5f5f70da8ce8a8ac5040f3413a0ffe9
Alin Voinea <alin@eaudeweb.ro>
2015-05-26 10:52:39 +0300

    Refs #25755 - Add deprecation warning

--- eea/pdf/async.py
+++ eea/pdf/async.py
@@ -1,10 +1,8 @@
 """ aysnc module - BBB
 """
-
-from eea.converter.async import ContextWrapper
-
-
 def make_async_pdf(*args, **kwargs):
-    """ """
-    print args
-    print kwargs
+    """ BBB """
+    import warnings
+    warnings.warn("eea.pdf.async.make_async_pdf is deprecated. "
+                  "Please use eea.converter.async.run_async_job instead",
+                  DeprecationWarning)
